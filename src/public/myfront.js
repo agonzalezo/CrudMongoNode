90c66269daf4e902f12e52136fb6432e76f65673
Alex <mexi1415@gmail.com>
2019-11-01 23:20:27 -0500

    version 1.0.3

--- src/public/myfront.js
+++ src/public/myfront.js
@@ -1,5 +1,4 @@
-console.log("Empieza la busqueda");
-// alert("LLame al js");
+console.log("Entramos al JS");
 
 function buscar(){
 let color = document.getElementById("txtcolor").value;
@@ -12,18 +11,18 @@ window.location=get_url;
 function agregar(){
 var xhr = new XMLHttpRequest();
 var url = "/cars/add";
-xhr.open("POST", url, true);
+xhr.open("POST", url, false);
 xhr.setRequestHeader("Content-Type", "application/json");
-xhr.onreadystatechange = function (e) {
-console.log("Primer log "+xhr.responseText);
+xhr.onreadystatechange = (e)=> {
+    alert(xhr.responseText)
 };
 let marca = document.getElementById("txtmarca").value;
 let modelo = document.getElementById("txtmodel").value;
 let ano = document.getElementById("txtano").value;
 let color2 = document.getElementById("txtcolor2").value;
 var data = JSON.stringify({"brand":marca, "model":modelo, "year":ano, "color":color2});
+console.log("Enviando Request "+data);
 xhr.send(data);
-console.log("Respuesta"+ xhr.responseText);
-alert(xhr.responseText)
+console.log("Response "+ xhr.responseText);
 //window.location="/api/carslist";
 }
